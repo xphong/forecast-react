c949f3ead2a82aa84c624bc4bd233d8d3e784c5e
Phong Huynh <phuynh@architech.ca>
2016-03-10 14:00:08 -0500

    Add search action and view dispatcher

--- src/scripts/actions/AppActions.js
+++ src/scripts/actions/AppActions.js
@@ -1,7 +1,12 @@
 var AppDispatcher = require('../dispatcher/AppDispatcher');
 
 var AppActions = {
-
+  searchWeather: function(item){
+    AppDispatcher.handleViewAction({
+      actionType: 'SEARCH_WEATHER',
+      item: item
+    })
+  }
 };
 
 module.exports = AppActions;
--- src/scripts/dispatcher/AppDispatcher.js
+++ src/scripts/dispatcher/AppDispatcher.js
@@ -1,6 +1,14 @@
 var Dispatcher = require('flux').Dispatcher;
 var assign = require('react/lib/Object.assign');
 
-var AppDispatcher = new Dispatcher();
+var AppDispatcher = assign(new Dispatcher(), {
+  handleViewAction: function(action){
+    console.log('action', action);
+    this.dispatch({
+      source: 'VIEW_ACTION',
+      action: action
+    })
+  }
+});
 
 module.exports = AppDispatcher;
